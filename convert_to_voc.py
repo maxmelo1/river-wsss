8889fdbeb83384f1bbf9807a01136466a60d500a
Max <max.mjm.melo@gmail.com>
2022-06-19 22:44:15 -0400

    classification task json object
    VOC style json object for classification task

--- convert_to_voc.py
+++ convert_to_voc.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+import json
 
 pix_cls = (128, 0, 0) #aeroplane in VOC
 
@@ -39,6 +40,41 @@ def save_files(img_name, mask_name):
     mask = Image.fromarray(new_mask).convert('RGB')
     mask.save('dataset2/SegmentationClass/'+new_mask_name+".png")
 
+def count_river(mask_ids):
+    c=0
+    for mask_train in mask_ids:
+        mask = np.array(Image.open(mask_train).convert("L"), dtype=np.float32)
+        c += np.sum(mask) > 0
+    
+    return c
+
+def save_dict(train_size, val_size):
+    print(type(train_size))
+    voc_dict = {"train":{
+                        "river": int(train_size)
+                    }, 
+                "validation":{
+                        "river": int(val_size)
+                    }, 
+                "classes": 1,
+                "class_names":["river"],
+                "class_dic":{"river": 0},
+                "color_dict": {
+                    "background": [
+                        0,
+                        0,
+                        0
+                    ],
+                    "river": [
+                        128,
+                        0,
+                        0
+                    ]}
+    }
+
+    with open('data/VOC_2012.json', 'w') as f:
+        json.dump(voc_dict, f)
+
 
 mask_train_ids = np.array([os.path.join(MASK_TRAIN_PATH, x) for x in sorted_fns(MASK_TRAIN_PATH)])
 img_train_ids = np.array([os.path.join(IMAGE_TRAIN_PATH, x) for x in sorted_fns(IMAGE_TRAIN_PATH)])
@@ -64,17 +100,19 @@ ftrain = open('data/train.txt', 'a')
 ftrain_aug = open('data/train_aug.txt', 'a') 
 #TODO implementar o boundaries para train_aug
 
-for img_train, mask_train in zip(img_train_ids, mask_train_ids):
-    ftrain.write(img_train.split('/')[3].split('.')[0]+'\n')
+# for img_train, mask_train in zip(img_train_ids, mask_train_ids):
+#     ftrain.write(img_train.split('/')[3].split('.')[0]+'\n')
 
-    save_files(img_train, mask_train)
+#     save_files(img_train, mask_train)
 
-for img_val , mask_val in zip(img_val_ids, mask_val_ids):
-    fval.write(img_val.split('/')[3].split('.')[0]+'\n')
+# for img_val , mask_val in zip(img_val_ids, mask_val_ids):
+#     fval.write(img_val.split('/')[3].split('.')[0]+'\n')
 
-    save_files(img_train, mask_val)
+#     save_files(img_train, mask_val)
 
 fval.close()
 ftrain.close()
 ftrain_aug.close()
 
+
+save_dict(count_river(mask_train_ids), count_river(mask_val_ids))
